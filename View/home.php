3d045a47524d57c07c5ff283e24313421ef07ba9
ThomasGrandmagnac <thomasgrandmagnac@gmail.com>
2016-06-01 01:59:08 +0200

    Integration du header et ajout des images en .svg et du logo.png

--- View/home.php
+++ View/home.php
@@ -2,10 +2,26 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="../assets/css/">
+    <title>DWIT</title>
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
 </head>
 <body>
-
+<header>
+    <nav>
+        <ul>
+            <li><a href=""><img src="../assets/img/cancel.svg" alt="Add a dwit">Add a dwit</a></li>
+            <li><a href=""><img src="../assets/img/logo.png" alt="Logo de DWIT"></a></li>
+            <li>
+                <ul>
+                    <li><a href=""><img src="../assets/img/magnifying-glass.svg" alt="Search">Search</a></li>
+                    <li><a href=""><img src="../assets/img/chat-2.svg" alt="Notifications">Notification</a></li>
+                    <li><a href=""><img src="../assets/img/avatar.svg" alt="Account">My account</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
 </body>
 </html>
