f667e7e7933600b05a26e31d6ced35b10248d6bf
quengeorges <quenting92@gmail.com>
2016-05-31 18:09:45 +0200

    fix bug

--- View/connection.php
+++ View/connection.php
@@ -8,7 +8,7 @@
 <?php
 if (!isset($_SESSION['login'])) {
     ?>
-    <form action="./index?route=connect" method="post">
+    <form action="./index.php?route=connect" method="post">
         <input type="text" name="pseudo">
         <input type="password" name="password">
         <input type="submit">
