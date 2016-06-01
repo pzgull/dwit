3b9b28821a89e4cedf61569fc5d2a10d0b028bb9
Tom <Tom>
2016-06-02 01:27:06 +0200

    Intégration : logo et première section faite

--- View/home.php
+++ View/home.php
@@ -8,67 +8,84 @@
     <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<header>
-    <nav>
-        <ul>
-            <li><a href="#"><img src="../assets/img/svg/cancel.svg" alt="Add a dwit">Add a dwit</a></li>
-            <li><a href="#"><img src="../assets/img/logo.png" alt="Logo de DWIT"></a></li>
-            <li>
-                <ul>
-                    <li><a href="#"><img src="../assets/img/svg/magnifying-glass.svg" alt="Search">Search</a></li>
-                    <li><a href="#"><img src="../assets/img/svg/chat-2.svg" alt="Notifications">Notification</a></li>
-                    <li><a href="#"><img src="../assets/img/svg/avatar.svg" alt="Account">My account</a></li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
-</header>
-<section class="hot-challenge">
-    <h1>Hot Challenge</h1>
-    <div class="wrapper">
-    <article>
-        <h2>Subway bag <span>challenge</span></h2>
-        <img src="../assets/img/image1.png" alt="Image 1">
-        <h3>Author : Jean Bon</h3>
-        <ul>
-            <li>
-                <ul>
-                    <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
-                    <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
-                </ul>
-            </li>
-            <li>Musical<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
-            <li>112<img src="../assets/img/svg/heart.svg" alt="Like"></li>
-        </ul>
-    </article>
-    <article>
-        <h2>Kill shabat <span>challenge</span></h2>
-        <img src="../assets/img/image1.png" alt="Image 2">
-        <h3>Author : Jean Bon</h3>
-        <ul>
-            <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
-            <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
-            <li>Racial<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
-            <li>666<img src="../assets/img/svg/heart.svg" alt="Like"></li>
-        </ul>
-    </article>
-    <article>
-        <h2>Yann's head <span>challenge</span></h2>
-        <img src="../assets/img/image3.png" alt="Image 3">
-        <h3>Author : Jean Bon</h3>
-        <ul>
-            <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
-            <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
-            <li>Wtf !?<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
-            <li>4<img src="../assets/img/svg/heart.svg" alt="Like"></li>
-        </ul>
-    </article>
-    </div>
-</section>
-<!--End of Hot Challenge Section-->
-<section class="recent-challenge">
-
-</section>
-<!--End of Recent Challenge Section-->
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#"><img src="../assets/img/svg/cancel.svg" alt="Add a dwit">Add a dwit</a></li>
+                <li><a href="#"><img src="../assets/img/logo.png" alt="Logo de DWIT"></a></li>
+                <li>
+                    <ul>
+                        <li><a href="#"><img src="../assets/img/svg/magnifying-glass.svg" alt="Search">Search</a></li>
+                        <li><a href="#"><img src="../assets/img/svg/chat-2.svg" alt="Notifications">Notification</a></li>
+                        <li><a href="#"><img src="../assets/img/svg/avatar.svg" alt="Account">My account</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <section class="hot-challenge">
+        <h1>Hot Challenge</h1>
+        <div class="wrapper">
+        <article>
+            <h2>Subway bag <span>challenge</span></h2>
+            <img src="../assets/img/image1.png" alt="Image 1">
+            <div class="author-article">
+                <h3>Author : Jean Bon</h3>
+                <p>28/05/2016</p>
+            </div>
+            <ul>
+                <li>
+                    <ul>
+                        <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
+                        <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
+                    </ul>
+                </li>
+                <li>Musical<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
+                <li>112<img src="../assets/img/svg/heart.svg" alt="Like"></li>
+            </ul>
+        </article>
+        <article>
+            <h2>GIF <span>challenge</span></h2>
+            <img src="../assets/img/image2.gif" alt="Image 2">
+            <div class="author-article">
+                <h3>Author : Jean Bon</h3>
+                <p>29/05/2016</p>
+            </div>
+            <ul>
+                <li>
+                    <ul>
+                        <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
+                        <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
+                    </ul>
+                </li>
+                <li>Art<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
+                <li>112<img src="../assets/img/svg/heart.svg" alt="Like"></li>
+            </ul>
+        </article>
+        <article>
+            <h2>Yann's head <span>challenge</span></h2>
+            <img src="../assets/img/image3.png" alt="Image 3">
+            <div class="author-article">
+                <h3>Author : Jean Bon</h3>
+                <p>30/05/2016</p>
+            </div>
+            <ul>
+                <li>
+                    <ul>
+                        <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
+                        <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
+                    </ul>
+                </li>
+                <li>Wtf!?<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
+                <li>112<img src="../assets/img/svg/heart.svg" alt="Like"></li>
+            </ul>
+        </article>
+        </div>
+    </section>
+    <!--End of Hot Challenge Section-->
+    <section class="recent-challenge">
+        <h1>Recent challenges</h1>
+    </section>
+    <!--End of Recent Challenge Section-->
 </body>
 </html>
