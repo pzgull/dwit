f0607517eaf934fc66fdb029d8f96266ef39eaf4
ThomasGrandmagnac <thomasgrandmagnac@gmail.com>
2016-06-02 09:55:10 +0200

    Intégration : ajout de l'hyperlien sur les articles et ajustement des articles à la même taille.

--- View/home.php
+++ View/home.php
@@ -26,60 +26,66 @@
     <section class="hot-challenge">
         <h1>Hot Challenge</h1>
         <div class="wrapper">
-        <article>
-            <h2>Subway bag <span>challenge</span></h2>
-            <img src="../assets/img/image1.png" alt="Image 1">
-            <div class="author-article">
-                <h3>Author : Jean Bon</h3>
-                <p>28/05/2016</p>
-            </div>
-            <ul>
-                <li>
+            <a href="">
+                <article>
+                    <h2>Subway bag <span>challenge</span></h2>
+                    <img src="../assets/img/image1.png" alt="Image 1">
+                    <div class="author-article">
+                        <h3>Author : Jean Bon</h3>
+                        <p>28/05/2016</p>
+                    </div>
                     <ul>
-                        <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
-                        <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
+                        <li>
+                            <ul>
+                                <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
+                                <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
+                            </ul>
+                        </li>
+                        <li>Musical<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
+                        <li>112<img src="../assets/img/svg/heart.svg" alt="Like"></li>
                     </ul>
-                </li>
-                <li>Musical<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
-                <li>112<img src="../assets/img/svg/heart.svg" alt="Like"></li>
-            </ul>
-        </article>
-        <article>
-            <h2>GIF <span>challenge</span></h2>
-            <img src="../assets/img/image2.gif" alt="Image 2">
-            <div class="author-article">
-                <h3>Author : Jean Bon</h3>
-                <p>29/05/2016</p>
-            </div>
-            <ul>
-                <li>
+                </article>
+            </a>
+            <a href="">
+                <article>
+                    <h2>GIF <span>challenge</span></h2>
+                    <img src="../assets/img/image2.gif" alt="Image 2">
+                    <div class="author-article">
+                        <h3>Author : Jean Bon</h3>
+                        <p>29/05/2016</p>
+                    </div>
                     <ul>
-                        <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
-                        <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
+                        <li>
+                            <ul>
+                                <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
+                                <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
+                            </ul>
+                        </li>
+                        <li>Art<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
+                        <li>112<img src="../assets/img/svg/heart.svg" alt="Like"></li>
                     </ul>
-                </li>
-                <li>Art<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
-                <li>112<img src="../assets/img/svg/heart.svg" alt="Like"></li>
-            </ul>
-        </article>
-        <article>
-            <h2>Yann's head <span>challenge</span></h2>
-            <img src="../assets/img/image3.png" alt="Image 3">
-            <div class="author-article">
-                <h3>Author : Jean Bon</h3>
-                <p>30/05/2016</p>
-            </div>
-            <ul>
-                <li>
+                </article>
+            </a>
+            <a href="">
+                <article>
+                    <h2>Yann's head <span>challenge</span></h2>
+                    <img src="../assets/img/image3.png" alt="Image 3">
+                    <div class="author-article">
+                        <h3>Author : Jean Bon</h3>
+                        <p>30/05/2016</p>
+                    </div>
                     <ul>
-                        <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
-                        <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
+                        <li>
+                            <ul>
+                                <li>16<img src="../assets/img/svg/like.svg" alt="Success"></li>
+                                <li>23<img src="../assets/img/svg/dislike.svg" alt="Fail"></li>
+                            </ul>
+                        </li>
+                        <li>Wtf!?<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
+                        <li>112<img src="../assets/img/svg/heart.svg" alt="Like"></li>
                     </ul>
-                </li>
-                <li>Wtf!?<img src="../assets/img/svg/headphones.svg" alt="Category"></li>
-                <li>112<img src="../assets/img/svg/heart.svg" alt="Like"></li>
-            </ul>
-        </article>
+                </article>
+            </a>
         </div>
     </section>
     <!--End of Hot Challenge Section-->
