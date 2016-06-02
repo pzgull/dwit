763ae19cf424fd893fa5b851f2288d2a4549edd9
quengeorges <quenting92@gmail.com>
2016-06-02 10:32:41 +0200

    add bdd

---
+++ dwit.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 02, 2016 at 08:31 AM
+-- Server version: 5.5.49-log
+-- PHP Version: 7.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dwit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorie`
+--
+
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `name`) VALUES
+(1, 'Art'),
+(2, 'Crafting'),
+(3, 'Cooking'),
+(4, 'Dance'),
+(5, 'Gaming'),
+(6, 'Musical'),
+(7, 'Sport'),
+(8, 'Wtf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `defi`
+--
+
+CREATE TABLE IF NOT EXISTS `defi` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `success` int(11) NOT NULL,
+  `failed` int(11) NOT NULL,
+  `like` int(11) NOT NULL,
+  `id_categorie` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `defi`
+--
+
+INSERT INTO `defi` (`id`, `titre`, `description`, `img`, `success`, `failed`, `like`, `id_categorie`, `date`) VALUES
+(1, 'fils de puterie', 'niquer des meres', '', 1, 0, 0, 8, '2016-05-31 17:04:06'),
+(2, 'Bonjour', 'dire bonjour', '', 1, 0, 45, 3, '2016-06-02 08:18:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `defi_user`
+--
+
+CREATE TABLE IF NOT EXISTS `defi_user` (
+  `id_user` int(11) NOT NULL,
+  `id_defi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `defi_user`
+--
+
+INSERT INTO `defi_user` (`id_user`, `id_defi`) VALUES
+(1, 1),
+(1, 2),
+(2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `pseudo` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `right` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `pseudo`, `password`, `right`) VALUES
+(1, 'jean', 'jean', 1),
+(2, 'vincent', 'vincent', 0),
+(3, 'vincent', 'vincent', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `defi`
+--
+ALTER TABLE `defi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `defi`
+--
+ALTER TABLE `defi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
