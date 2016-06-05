b4bc103d65a230826cc572ce5fb84a52ca1981b0
quengeorges <quenting92@gmail.com>
2016-06-05 19:55:49 +0200

    fix search

--- View/home.php
+++ View/home.php
@@ -20,7 +20,7 @@
                 </li>
                 <li>
                     <ul>
-                        <li><a href="#"><img src="../assets/img/svg/magnifying-glass.svg" alt="Search">Search</a></li>
+                        <li><a href="#" id="search"><img src="../assets/img/svg/magnifying-glass.svg" alt="Search">Search</a></li>
                         <li><a href="#"><img src="../assets/img/svg/chat-2.svg" alt="Notifications">Notification</a></li>
                         <li><a href="#" id="account"><img src="../assets/img/svg/avatar.svg" alt="Account">My account</a></li>
                     </ul>
@@ -66,6 +66,37 @@
                 </div>
             </form>
         </div>
+    </section>
+    <section class="search">
+        <form action="">
+            <div class="search-frame">
+                <div class="search-for">
+                    <label for="searchFor">You are searching for:</label>
+                    <input type="search" id="searchFor">
+                </div>
+                <div class="sorted-by">
+                    <p>Sorted by:</p>
+                    <label for="byDate"><input type="radio" id="byDate">Date</label>
+                    <p>or</p>
+                    <label for="byLike"><input type="radio" id="byLike">Like</label>
+                </div>
+            </div>
+            <div class="row categories">
+                <p>Categories:
+                    <label id="icon-art"><input type="radio" name="category" id="art" value="art"><i class="icon-art"></i>Art</label><!--ART-->
+                    <label id="icon-cooking"><input type="radio" name="category" id="cooking" value="cooking"><i class="icon-cooking"></i>Cooking</label><!--COOKING-->
+                    <label id="icon-crafting"><input type="radio" name="category" id="crafting" value="crafting"><i class="icon-crafting"></i>Crafting</label><!--CRAFTING-->
+                    <label id="icon-dance"><input type="radio" name="category" id="dance" value="dance"><i class="icon-dance"></i>Dance</label><!--DANCE-->
+                    <label id="icon-gaming"><input type="radio" name="category" id="gaming" value="gaming"><i class="icon-gaming"></i>Gaming</label><!--GAMING-->
+                    <label id="icon-headphones"><input type="radio" name="category" id="musical" value="musical"><i class="icon-headphones"></i>Musical</label><!--MUSICAL-->
+                    <label id="icon-sport"><input type="radio" name="category" id="sport" value="sport"><i class="icon-sport"></i>Sport</label><!--SPORT-->
+                    <label id="icon-weird"><input type="radio" name="category" id="wtf!?" value="wtf!?"><i class="icon-weird"></i>WTF!?</label><!--WTF-->
+                </p>
+            </div>
+            <div class="button-search">
+                <input type="submit" value="Search" class="waves-effect waves-light btn">
+            </div>
+        </form>
     </section>
     <!--End of Add a DWIT Section-->
     <section class="account">
